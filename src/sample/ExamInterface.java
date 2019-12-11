701f9c826014d08ab1796aa217c2ccdb8aad9c71
schredderinsohr <rmgalileo@teleport.ch>
2019-12-11 13:15:43 +0100

    Added ArrayList so the delete Button works

--- src/sample/ExamInterface.java
+++ src/sample/ExamInterface.java
@@ -1,5 +1,9 @@
 package sample;
 
+import java.util.ArrayList;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
@@ -13,8 +17,45 @@ public class ExamInterface extends Parent {
 	
 	
 	Exam exam = new Exam("Physik");
+
 	int rowCount=1;
 	
+	
+	private ArrayList<MarkInterface> markInterfaceArrayList = new ArrayList<>();
+	
+	
+		
+	
+	
+	
+	
+	class DeleteButtonEvent implements EventHandler<ActionEvent> {
+		
+		private final int number;
+		GridPane gridPane;
+		DeleteButtonEvent(int number, GridPane grid){
+			this.gridPane = grid;
+		
+		
+		this.number= number;
+		}
+		
+		@Override
+		
+		public void handle (ActionEvent event) {
+			
+		gridPane.getChildren().removeIf(node -> GridPane.getRowIndex(node) == number);
+		markInterfaceArrayList.remove(number);
+		
+			
+	//	exam.deleteMark(number);
+		
+		
+			
+		}
+		
+		
+	}
 	
 	public ExamInterface() {
 		
@@ -25,19 +66,19 @@ public class ExamInterface extends Parent {
 		gridPane.setHgap(10);    
 		gridPane.setVgap(10);
 		
-		MarkInterface mark1 = new MarkInterface(exam);
+	//	MarkInterface mark1 = new MarkInterface(exam);
 		
-		Button deleteButton = new Button("Löschen");
+	//	Button deleteButton = new Button("Löschen");
 		Button neueNoteButton = new Button("neue Note");
 		
-		//CheckBox cb1 = new CheckBox();
+	
 
 		//------- Set GridPane------
 		
-		gridPane.add(mark1, 0, 1);
-		gridPane.add(deleteButton, 1, 0);
-		gridPane.add(neueNoteButton, 0 , 0);
-		gridPane.add(new CheckBox(),1,1);
+		//gridPane.add(mark1, 0, 1);
+		//gridPane.add(deleteButton, 1, 1);
+		  gridPane.add(neueNoteButton, 0 , 0);
+		//gridPane.add(new CheckBox(),1,1);
 		
 		
 		
@@ -46,39 +87,17 @@ public class ExamInterface extends Parent {
 		
 		neueNoteButton.setOnAction(e -> {
 		
+			markInterfaceArrayList.add(new MarkInterface(exam));
 			
-			gridPane.add(new MarkInterface(exam),0,rowCount+1);
+			gridPane.add(createDeleteButton(markInterfaceArrayList.size()+1, gridPane), 1, markInterfaceArrayList.size()+1);
 			
-			gridPane.add(new CheckBox(),1,rowCount+1);
-		
+			gridPane.add(markInterfaceArrayList.get(markInterfaceArrayList.size()-1),0,markInterfaceArrayList.size()+1);
+			
+	  	
 			
 
-			rowCount = rowCount+1;
 			
 	
-		});
-		
-		/// -----Action Button (delete Checkbox)--------
-		
-			deleteButton.setOnAction(e -> {
-				
-				if (new CheckBox().isSelected()){
-					
-				//	gridPane.getChildren().removeIf(node -> GridPane.getRowIndex());
-				}
-				
-				/*	if(new CheckBox().isSelected()) {
-					
-					gridPane.getChildren().removeIf(node -> GridPane.getRowIndex(node) == 2);
-				}*/
-				
-					
-				//gridPane.getChildren().removeRow();
-				//	gridPane.removeAll();
-				//getSelectedItems().clear();
-			
-				//rowCount = rowCount-1;
-			
 		});
 		
 	
@@ -96,4 +115,18 @@ public class ExamInterface extends Parent {
 		rowCount++;
 		return rowCount;
 	}
+	
+	public Button createDeleteButton (int number, GridPane gridPane) {
+		
+		
+		Button deleteButton = new Button ("Löschen");
+		deleteButton.setOnAction(new DeleteButtonEvent(number, gridPane));
+		
+		
+		
+		return deleteButton;
+	}
+	
+	
+		
 }
